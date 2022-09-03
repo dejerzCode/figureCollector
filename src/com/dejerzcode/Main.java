4e8a494f09a17ac50d925a937c4980f7bd7eb289
dejerzCode <dejerzcode@gmail.com>
2022-09-03 15:33:07 -0600

    Commented lines on main

--- src/com/dejerzcode/Main.java
+++ src/com/dejerzcode/Main.java
@@ -1,6 +1,7 @@
 package com.dejerzcode;
 
 import com.dejerzcode.entities.Circle;
+import com.dejerzcode.entities.Figure;
 import com.dejerzcode.entities.Square;
 import com.dejerzcode.entities.Triangle;
 import com.dejerzcode.utilities.MyActions;
@@ -8,7 +9,10 @@ import com.dejerzcode.utilities.MyActions;
 public class Main extends MyActions {
 
     public static void main(String[] args) {
-        Triangle newTriangle = new Triangle();
+        //Figure triangle2 = new Triangle();
+        //Triangle newTriangle = new Triangle();
+        //Un objeto de clase padre se puede inicializar con el constructor de una clase hija.
+        Figure newTriangle = new Triangle();
         Square newSquare = new Square();
         Circle newCircle = new Circle();
         boolean exit = false;
@@ -20,6 +24,8 @@ public class Main extends MyActions {
                     break;
                 case 1:
                     setDimentions(newTriangle);
+                    //Se puede reducir a una escala el área.
+                    //newTriangle.zoom(50);
                     print("Area: " + newTriangle.getArea());
                     break;
                 case 2:
